7b4f73b690912193795e38d6ba327ffc2a7fa005
Deepanshu Oswal <77071794+d-o-2021@users.noreply.github.com>
2022-10-26 22:51:25 +0530

    Create 1051. Height Checker.java

---
+++ 1051. Height Checker.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int arr[]=new int[heights.length];
+        for(int i=0;i<heights.length;i++)arr[i]=heights[i];
+        Arrays.sort(arr);
+        int sum=0;
+        for(int i=0;i<heights.length;i++)
+        {
+            if(heights[i]!=arr[i])sum++;
+        }
+        return sum;
+    }
+}
