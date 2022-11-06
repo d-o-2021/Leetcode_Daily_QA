4f5ad8c74331868cf4703d1afba78d041ff0dbc3
Deepanshu Oswal <77071794+d-o-2021@users.noreply.github.com>
2022-11-06 23:12:53 +0530

    Create 1287. Element Appearing More Than 25% In Sorted Array.java

---
+++ 1287. Element Appearing More Than 25% In Sorted Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int per=arr.length/4;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==arr[i+per])
+            {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
