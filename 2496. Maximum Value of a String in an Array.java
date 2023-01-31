5bdc096e145178f4580104b63cd7b6a1cf55cc5d
Deepanshu Oswal <77071794+d-o-2021@users.noreply.github.com>
2023-01-31 18:00:41 +0530

    Update 2496. Maximum Value of a String in an Array.java

--- 2496. Maximum Value of a String in an Array.java
+++ 2496. Maximum Value of a String in an Array.java
@@ -1,3 +1,21 @@
+// # Intuition
+// First thought was to check each string one by one
+// <!-- Describe your first thoughts on how to solve this problem. -->
+
+// # Approach
+// Second thought is to use exception handling concept to return result
+// # Logic : 
+// If a string contains any alphabet than it can not be converted to integer and hence if that string only contains number we return that integer otherwise we return length of string. 
+// <!-- Describe your approach to solving the problem. -->
+
+// # Complexity
+// - Time complexity: O(n)
+// <!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+// - Space complexity: O(1)
+// <!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+// # Code
 class Solution {
     public int len(String s)
     {
