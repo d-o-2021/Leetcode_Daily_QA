5eadf26f1ab1a8122bd01fb26e9fceee5a13df10
Deepanshu Oswal <77071794+d-o-2021@users.noreply.github.com>
2023-02-20 16:50:43 +0530

    Create 1913. Maximum Product Difference Between Two Pairs.java

---
+++ 1913. Maximum Product Difference Between Two Pairs.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        int len=nums.length-1;
+        return (nums[len]*nums[len-1])-(nums[0]*nums[1]);
+    }
+}
