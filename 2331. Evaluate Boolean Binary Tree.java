a6871d92431d9ab9b62ca6e0f23ee53e9c1485c5
Deepanshu Oswal <77071794+d-o-2021@users.noreply.github.com>
2024-05-17 00:38:14 +0530

    Update 2331. Evaluate Boolean Binary Tree.java

--- 2331. Evaluate Boolean Binary Tree.java
+++ 2331. Evaluate Boolean Binary Tree.java
@@ -27,3 +27,41 @@ class Solution {
         return solve(root);
     }
 }
+
+
+
+
+
+
+// case 2/**
+ // * Definition for a binary tree node.
+ // * public class TreeNode {
+ // *     int val;
+ // *     TreeNode left;
+ // *     TreeNode right;
+ // *     TreeNode() {}
+ // *     TreeNode(int val) { this.val = val; }
+ // *     TreeNode(int val, TreeNode left, TreeNode right) {
+ // *         this.val = val;
+ // *         this.left = left;
+ // *         this.right = right;
+ // *     }
+ // * }
+ // */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        return solve(root);
+    }
+    public boolean solve(TreeNode root){
+        if(root.left ==null && root.right ==null)return root.val == 0 ? false:true;
+        boolean left = solve(root.left);
+        boolean right = solve(root.right);
+        if(root.val == 2){
+            return left || right;
+        }
+        else if(root.val == 3){
+            return left && right;
+        }
+        return false;
+    }
+}
