11306f0821ec56d5447bc7a8c1bf25248be60826
Deepanshu Oswal <77071794+d-o-2021@users.noreply.github.com>
2024-07-23 00:58:48 +0530

    Update 2418. Sort the People.java

--- 2418. Sort the People.java
+++ 2418. Sort the People.java
@@ -24,3 +24,21 @@ class Solution {
         return names;
     }
 }
+
+
+// using map
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        Map<Integer,String> map = new HashMap<>();
+        for(int i=0; i < heights.length; i++){
+            map.put(heights[i],names[i]);
+        }
+        Arrays.sort(heights);
+        int count=0;
+        for(int i=heights.length-1; i>=0 ;i--){
+            names[count++]=map.get(heights[i]);
+        }
+        return names;
+
+    }
+}
